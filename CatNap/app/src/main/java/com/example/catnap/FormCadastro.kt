a9f83a2ba5cdc5fa2ab9e6aee99df3fe39187e3d
Juan Servelo <91964551+JuanServelo@users.noreply.github.com>
2024-10-06 21:33:34 -0300

    Atualização telas de login e cadastro
    Finalização da tela de cadastro e realização de ajustes na tela de login.

--- CatNap/app/src/main/java/com/example/catnap/FormCadastro.kt
+++ CatNap/app/src/main/java/com/example/catnap/FormCadastro.kt
@@ -1,12 +1,17 @@
 package com.example.catnap
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class FormCadastro : AppCompatActivity() {
+
+    private lateinit var text_tela_login: TextView;
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +21,18 @@ class FormCadastro : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        IniciarComponentes()
+
+        text_tela_login.setOnClickListener {
+            val intent = Intent(this@FormCadastro, FormLogin::class.java)
+            startActivity(intent)
+        }
+
     }
+
+    private fun IniciarComponentes() {
+        text_tela_login = findViewById<TextView>(R.id.texto_login)
+    }
+
 }
